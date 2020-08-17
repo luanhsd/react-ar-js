0945858d706b552763b84d201341927bde65b9ac
Luan Dantas <luan_h.s.d@hotmail.com>
2020-08-16 23:02:05 -0300

    teste do AR.JS no React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <a-scene vr-mode-ui='enabled:false' embedded arjs='debugUIEnabled:false;'>
+      <a-marker present='hiro'>
+        <a-box color='red' />
+      </a-marker>
+      <a-entity camera />
+    </a-scene>
+  );
+}
+
+export default App;
